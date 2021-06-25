acf5171af20ac5b637d97b8ace887af6660aecf4
snehasharma12 <sharma.sn@northeastern.edu>
2021-06-24 20:47:34 -0400

    modified code Developer-2 branch

--- time.py
+++ time.py
@@ -2,4 +2,9 @@ import time
 import datetime
 time = datetime.datetime.now()
 current_time = time.strftime("%H:%M:%S")
+date = datetime.date.today()
+day = datetime.date.today()
+current_day = day.strftime("%A")
 print("Time: ", current_time)
+print("date: ", date)
+print("day: ", current_day)
