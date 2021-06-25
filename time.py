444ba5ce22531e20f2a37a3b40277fd7e646a7f6
snehasharma12 <sharma.sn@northeastern.edu>
2021-06-24 20:37:09 -0400

    modified code Developer-1 branch

--- time.py
+++ time.py
@@ -2,4 +2,6 @@ import time
 import datetime
 time = datetime.datetime.now()
 current_time = time.strftime("%H:%M:%S")
+date = datetime.date.today()
 print("Time: ", current_time)
+print("Date: ", date)
